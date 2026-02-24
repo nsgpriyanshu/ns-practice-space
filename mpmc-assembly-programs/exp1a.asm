b3bc847f4e2a2b98d19a437f220961b4119363b7
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2026-02-24 19:06:26 +0530

    Delete mpmc-assembly-programs directory

--- mpmc-assembly-programs/exp1a.asm
+++
@@ -1,9 +0,0 @@
-LDA 2000H     ; Load first number into A
-MOV B, A      ; Move to B (optional clarity)
-
-LDA 2001H     ; Load second number
-ADD B         ; A = A + B
-
-STA 2002H     ; Store result
-
-HLT
